56857e70bb42d24a71dfaa8812469dedfb7aa994
atsushi0919 <hatake0919@yahoo.co.jp>
2022-01-11 11:52:48 +0900

    C#8.0の書き方に変更 array[^1]で最後の要素を取り出す

--- array_utilization_primer_02-04_push_back/Program.cs
+++ array_utilization_primer_02-04_push_back/Program.cs
@@ -15,7 +15,10 @@ namespace array_utilization_primer_02_04_push_back
             int b = int.Parse(Console.ReadLine());
 
             Array.Resize(ref array, array.Length + 1);
-            array[array.Length - 1] = b;
+            // paiza 提出用
+            // array[array.Length - 1] = b;
+            // C# 8.0 以降の書き方
+            array[^1] = b;
 
             Console.WriteLine(String.Join("\n", array));
         }
